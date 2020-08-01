f3c3527b3348ba8ae842a3df743db4c29d1e0cc1
jeff8chen <jeff8chen@yahoo.com>
2020-08-01 13:38:22 -0400

    add more comments

--- find_meteors.py
+++ find_meteors.py
@@ -1,3 +1,4 @@
+
 import math
 import requests
 
@@ -16,20 +17,26 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
-    
-if __name__ == '__main__':
-    my_loc = (29.424122, -98.493628)
 
-    meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
-    meteor_data = meteor_resp.json()
+my_loc = (40.2892, -75.2149)
 
-    for meteor in meteor_data:
-        if not ('reclat' in meteor and 'reclong' in meteor): continue
-        meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                       float(meteor['reclong']),
-                                       my_loc[0],
-                                       my_loc[1])
+meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
+meteor_data = meteor_resp.json()
+# meteor_data is a list of dictionary as follows:
+#{"name":"Aachen","id":"1","nametype":"Valid","recclass":"L5","mass":"21","fall":"Fell","year":"1880-01-01T00:00:00.000",
+#"reclat":"50.775000","reclong":"6.083330","geolocation":{"type":"Point","coordinates":[6.08333,50.775]}}
 
-    meteor_data.sort(key=get_dist)
+#if no lat or long, then skip.
+#otherwise, calculate the distance, then add it to the dictionary meteor_data.
+for meteor in meteor_data:
+    if not ('reclat' in meteor and 'reclong' in meteor): continue
+    meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                   float(meteor['reclong']),
+                                   my_loc[0],
+                                   my_loc[1])
+meteor_data.sort(key=get_dist)
 
-    print(meteor_data[0:10])
+#print top 10 closest meteors.
+for m in meteor_data[0:10]:
+    print(m)
+    print("\n")
