681b1b231b954bf320b7ce5e629bd8d81b7a819b
jeff8chen <jeff8chen@yahoo.com>
2020-08-05 16:19:13 -0400

    update on 8/5

--- textfile_handler.py
+++ textfile_handler.py
@@ -1,4 +1,4 @@
-
+import csv, os.path, sys
 
 def read_to_string(filename):
     with open(filename) as input_file:
@@ -23,4 +23,23 @@ def writebyte_to_file(filename, bytes[])
         for b in bytes:
             output_file.write(b)
 
-    
+def read_from_csv(filename):
+    result =''
+    with open(filename, newline='') as f:
+        csv_reader = csv.reader(f)
+        try:
+            for row in csv_reader:
+                if result =='':
+                    result = row
+                else:
+                    result = result + "\n" + row
+        except csv.Error as ce:
+            sys.exit(f"the line {csv_reader.line_num} in the file {filename} has error: {ce} ")
+
+def write_to_csvfile(filename, list2write):
+    with open(filename, 'w', newline='') as f:
+        try:
+            csv_writer = csv.writer(f)
+            csv.writerrows(list2write)
+        except csv.Error as ce:
+            sys.exit(f"error in writing to the file {filename}: {ce}")
